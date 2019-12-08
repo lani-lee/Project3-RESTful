c6a9032eec3365dc9163c6d4b4a585571756d676
lani-lee <lee09113@stthomas.edu>
2019-12-08 11:23:03 -0600

    added docs folder

---
+++ docs/index.js
@@ -0,0 +1,21 @@
+
+var bounds = [	
+				[44.888288,-93.1779],
+				[44.991993,-93.005062]
+			 ]
+var corner1 = L.latLng(44.888288,-93.1779),
+	corner2 = L.latLng(44.991993,-93.005062),
+	boundss=L.latLngBounds(corner1,corner2);
+var map = L.map('mapid',{
+	maxBounds: boundss
+}).setView([44.949, -93.079],13);
+
+//L.latLngBounds(boundss).addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: 13,
+		maxZoom: 18,
+		id:'mapbox/streets-v11',
+		accessToken:'pk.eyJ1IjoiYW11ZDY5ODEiLCJhIjoiY2szdXltcGtjMDU5djNobHBqMzk4eG0zeCJ9.BsZLJkCaw2Bui_sh7DmdgQ'
+
+}).addTo(map);
